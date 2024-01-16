5b2c39b9709793df6e65a4126d115d9b90e342af
FarhanOSD <136696995+FarhanOSD@users.noreply.github.com>
2024-01-16 21:02:08 +0600

    Add files via upload

---
+++ name.py
@@ -0,0 +1,8 @@
+animals = [
+    "Lion", "Tiger", "Elephant", "Giraffe", "Zebra",
+    "Kangaroo", "Koala", "Panda", "Gorilla", "Monkey",
+    "Cheetah", "Leopard", "Hippopotamus", "Rhino", "Crocodile",
+    "Alligator", "Penguin", "Polar Bear", "Dolphin", "Whale",
+    "Shark", "Octopus", "Sloth", "Pangolin", "Camel",
+    "Parrot", "Peacock", "Giraffe", "Horse", "Dog"
+]
